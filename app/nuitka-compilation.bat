532a81bbdf44afc6e1971d1170f89fd3f10319d7
Mohammadreza Mohseni <mohamad.r.mohseni@gmail.com>
2024-12-17 07:52:18 +0330

    remove --standalone switch and enable pyqt6 plugin to suit the v2 app

--- app/nuitka-compilation.bat
+++ app/nuitka-compilation.bat
@@ -1,3 +1,3 @@
 @echo off
-python -m nuitka --standalone --onefile --windows-console-mode=disable --include-data-files=resources/mohseni.ico=resources/mohseni.ico --plugin-enable=pyqt5 --windows-icon-from-ico=resources/mohseni.ico --output-dir=dist --remove-output MaxBridge_Assistant.py
+python -m nuitka --onefile --windows-console-mode=disable --include-data-files=resources/mohseni.ico=resources/mohseni.ico --enable-plugins=pyqt6 --windows-icon-from-ico=resources/mohseni.ico --output-dir=dist --remove-output MaxBridge_Assistant_v2.py
 pause
